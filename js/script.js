2136be4041c44be508457fcd11629341639a5410
Connie <clai011@ucr.edu>
2018-04-10 18:20:22 -0700

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var logThis = "I'd like to learn the process of making an app, learn another language, and get another step closer to become a front-end developer etc. :D'";
 
-var myValue = confirm(logThis);
 
-console.log(logThis);
+var logInConsole = confirm(logThis);
+
+console.log(logInConsole);
+
+document.write("I think I really got it this time.");
