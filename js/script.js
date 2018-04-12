23d307ce858ca8e0a5fc063645c389bed683bd08
Connie <clai011@ucr.edu>
2018-04-11 20:21:31 -0700

    vars

--- js/script.js
+++ js/script.js
@@ -1,8 +1,65 @@
-var logThis = "I'd like to learn the process of making an app, learn another language, and get another step closer to become a front-end developer etc. :D'";
+
+/* var myName = "Connie";
+//myName = new String(myName);
+
+console.log(myName.toUpperCase());
 
 
-var logInConsole = confirm(logThis);
+ Must convert to Object to use .toUpperCase();
 
-console.log(logInConsole);
+JS type coersion
 
-document.write("I think I really got it this time.");
+var myName = 23;
+var myName = new Number(myName);  CONVERT PRIMITIVE TO OBJECT
+myName = new String(myName); //JS is case-sensitive, String = object, string = primitive;
+
+console.log(typeof(myName));
+
+lowercase "string" = primitive, not object;
+
+convert to obj;
+
+NOTES:
+3 ways to check if it's Primitive or Object:
+
+Method: typeof()
+
+new Object-Name()
+
+Uppercase in console
+
+
+*/
+
+
+
+//Undefined?
+// where were you expecting the value come from? define the variable!
+//where is it actually defined?
+
+
+/* let VARIABLES ARE NOT HOISTED
+
+console.log(myName, "one");
+{
+  let myName = "Connie";
+  console.log(myName, "Two");
+};
+
+console.log(myName, "Three");
+*/
+
+
+/* CONST VARIABLE */
+
+
+// BLOCKS --->SCOOOOOPES; GLOBAL/LOCAL VARIABLES!
+const myName = "C";
+
+console.log(myName, "one");
+{
+ myName = "Connie";
+  console.log(myName, "Two");
+};
+
+console.log(myName, "Three");
