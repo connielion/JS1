e38c58e243ebd6c3a8dae569c36febbe2679a44b
Connie <clai011@ucr.edu>
2018-04-16 17:32:10 -0700

    done

--- js/script.js
+++ js/script.js
@@ -1,32 +1,22 @@
 /*
-
+Asks for the user's first name
+Asks for the user's last name
+Logs the user's first name to the console
+Alerts the user's last name
 *Confirms the user's birthday and alerts the resulting birthday
 Include the app.js in an empty html page
-
 Add it to a repository's gh-pages branch so I can test it
 Send me the url of the gh-pages on Slack
+* Extra points if you use JavaScript's Date object for the birthday.
 */
 
-/* Date()
-let bday = '03/08/95'
-let rightNow = new Date(bday);
+var firstName = prompt('Your first name is...?');
+var lastName = prompt('Your last name is...?');
 
-alert(rightNow);
+console.log(firstName);
 
-*/
+alert(lastName);
 
-var FirstName = prompt("What is your first name?");
-
-var LastName = prompt("What is your last name?");
-
-var bDay = prompt("Your bday?");
-
-
-
-console.log(FirstName);
-
-alert(LastName);
-
-confirm(bDay);
-
-alert(bDay);
+var bday = prompt('Ur bday is...?');
+confirm.log(bday);
+alert(bday);
