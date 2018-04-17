b5c0f4cad8f457c5436d332703b44d9af7ce9605
Connie <clai011@ucr.edu>
2018-04-16 17:31:29 -0700

    done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-var logThis = "I'd like to learn the process of making an app, learn another language, and get another step closer to become a front-end developer etc. :D'";
+/*
+Asks for the user's first name
+Asks for the user's last name
+Logs the user's first name to the console
+Alerts the user's last name
+*Confirms the user's birthday and alerts the resulting birthday
+Include the app.js in an empty html page
+Add it to a repository's gh-pages branch so I can test it
+Send me the url of the gh-pages on Slack
+* Extra points if you use JavaScript's Date object for the birthday.
+*/
 
+var firstName = prompt('Your first name is...?');
+var lastName = prompt('Your last name is...?');
 
-var logInConsole = confirm(logThis);
+console.log(firstName);
 
-console.log(logInConsole);
+alert(lastName);
 
-document.write("I think I really got it this time.");
+var bday = prompt('Ur bday is...?');
+confirm.log(bday);
+alert(bday);
