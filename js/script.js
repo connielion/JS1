6f0d05a8ab084d3227825a240aa28088d95e0198
Connie <clai011@ucr.edu>
2018-04-17 21:45:25 -0700

    done 3

--- js/script.js
+++ js/script.js
@@ -1,22 +1,73 @@
 /*
-Asks for the user's first name
-Asks for the user's last name
-Logs the user's first name to the console
-Alerts the user's last name
-*Confirms the user's birthday and alerts the resulting birthday
-Include the app.js in an empty html page
-Add it to a repository's gh-pages branch so I can test it
-Send me the url of the gh-pages on Slack
-* Extra points if you use JavaScript's Date object for the birthday.
+
+Now that all your users at this point are at least 18, ask if they are less than 80 years old.
+Now ask the users who are between 18 and 80 if they like Star Wars. If they are over 80, ask if they like prunes.
+If they say they don't Star Wars, end the program. In all other cases, alert them with a message.
 */
 
-var firstName = prompt('Your first name is...?');
-var lastName = prompt('Your last name is...?');
+var askAge = prompt("What's your age?");
+var age = new Date(askAge);
+alert(age);
 
-console.log(firstName);
+if (age >= 18 ) { //over 18
+  console.log("Go ahead!");
+} else (age < 18) { //minor
+  alert("You're underage!");
+} else if (age >= 18) { //
+  prompt('Are you less than 80 years old?');
+} else if (age >18 && age <80) { // 18< age <80
+ prompt('Do you like Star Wars?');
+} else if (age >= 80){
+  prompt('Do you like prunes?');
+} else {
+  prompt('Are you Darth Vader?');
+}
 
-alert(lastName);
 
-var bday = prompt('Ur bday is...?');
-confirm.log(bday);
-alert(bday);
+
+
+
+
+
+
+
+/* =========================================================================================================
+
+Prompts your user for their name.
+Next, ask the user's favorite color. (Provide the user with the basic 10 colors as options i.e. red, orange, yellow, green, blue, pink, purple, black, white, and grey).
+Depending on the color, write 9 different responses for the possible answers. Include the user's name in the response message.
+======================================================================
+
+var askName = prompt('Your name is...?');
+var askColor = prompt("Pick favorite color: red, orange, yellow, green, blue, pink, purple, black, white, and grey.").
+
+switch {
+  case 1:
+  askColor === red;
+  break;
+  case 2:
+  askColor === orange;
+  break;
+  case 3:
+  askColor === yellow;
+  break;
+  case 4:
+  askColor === green;
+  break;
+  case 5:
+  askColor === blue;
+  break;
+  case 6:
+  askColor === pink;
+  break;
+  case 7:
+  askColor === purple;
+  break;
+  case 8:
+  askColor === black;
+  break;
+  case 9:
+  askColor === white;
+  break;
+}
+*/
