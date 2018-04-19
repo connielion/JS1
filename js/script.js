7cd701617d8ddb9a9b95bb6397e0af69850d2134
Connie <clai011@ucr.edu>
2018-04-18 18:12:54 -0700

    erased typo

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 let myStatus = prompt("Are you sleepy? (Enter: Yes/No; not case sensitive)");
 
-if (myStatus === "Y" || myStatus === "y" || myStatus == "yes" || myStatus == "Yes") {
-  let fizzbuzz = prompt("Were you trying to solve the FizzBuzz?")
+if (myStatus === "Y" || myStatus === "y" || myStatus === "yes" || myStatus === "Yes") {
+      let fizzbuzz = prompt("Were you trying to solve the FizzBuzz?")
 
-  if (fizzbuzz == "yes" || fizzbuzz == "Yes") {
-      alert("Nice!")
+      if (fizzbuzz == "yes" || fizzbuzz == "Yes") {
+          alert("Nice!")
 
-  }
-  else {
-    alert("Oh, ok.")
-  }
+      }
+      else {
+        alert("Oh, ok.")
+      }
 
-} else if (myStatus === "N" || myStatus === "n") {
+} else if (myStatus === "N" || myStatus === "n" || myStatus === "no" || myStatus === "No" ) {
     let movie = confirm("Wanna watch a movie?")
     if (movie === true) {
       alert("You go pick the movie. I'll make popcorns!")
     }
     else {
-      prompt("What would you like to do?")
+      prompt("What would you like to do? ")
     }
 }
-else {å
+else {
   alert("Go do some coding!");
 };
