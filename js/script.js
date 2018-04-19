dbca89009c753fa09ea3f5948814e88c9e31d4ab
Connie <clai011@ucr.edu>
2018-04-18 21:49:23 -0700

    color switch statement done

--- js/script.js
+++ js/script.js
@@ -1,58 +1,34 @@
-let emailArr;
-var yourEmail;
-
-while (true) {
-  //evaluates the truthyness of 'true'
-  var yourEmail = prompt('What is your email?');
-  emailArr = yourEmail.split("");
-  if (!(emailArr.includes('@'))) {
-    var yourEmail = prompt('That was not a valid email. Please try again.');
-  }
-  else if (!(emailArr.includes('.'))) {
-    var yourEmail = prompt('That was not a valid email. Please try again.');
-  }
-  else {
-    alert('Thanks for providing a valid email.');
-  }
-}
-
-
-//SWITCH STATEMENT - evaluates single parameter
-
 /*
-var numOfProjects = prompt(“How many projects do you have? You can choose a number 0-2.“);
-switch(parseInt(numOfProjects)) {
- case 0:
-     alert(“You won’t have a projects section on your page”);
-     break;
-    case 1:
-     alert(“You’ll only have one showing”);
-     break;
-    case 2:
-   alert(“You’ll only have one showing”);
-        break;
- default:
-   alert(’You didn\‘t follow the rules.’);
+
+Prompts your user for their name.
+Next, ask the user's favorite color. (Provide the user with the basic 10 colors as options i.e. red, orange, yellow, green, blue, pink, purple, black, white, and grey).
+Depending on the color, write 9 different responses for the possible answers. Include the user's name in the response message.
+*/
+
+var askName = prompt("Comó se llama?");
+
+var askColor = prompt("Cuál es tu color favorito? (LOWERCASE ONLY: red, orange, yellow, green, blue, pink, purple, black, white, and grey)");
+
+switch(askColor) {
+  case 'red':
+  case 'orange':
+  case 'purple':
+  case 'pink':
+  alert("Eww, " + askColor + "! You know what? You suck, " + askName + "!");
+  break;
+
+  case 'blue':
+  case 'yellow':
+  alert(`Hey, ${askName}! It's my favorite color too!`);
+  break;
+
+  case 'white':
+  case 'black':
+  case 'grey':
+  alert('You are boring.');
+  break;
+
+  default:
+  alert("YOU CAN'T ENGLISH?! ")
+
 }
-*/
-
-
-Ternary Operator = short-hand for If-Else
-
-// condition ? exp 1 : exp2
-/* let age = prompt("your age?")
-
-(parseInt(age))>=18 ? alert("Yay") : alert("Nay")
-
-*/
-
-let cars = ["jeep", "subaru", "honda"];
-//cars.length --> 3
-//cars.push("bmw") adds bmw to the end
-
-// splice(index #, # of items you want to remove, "new item")
-
-//splice needs 1 parameter
-
-
-//slice() with no argument returns exact copy of array
