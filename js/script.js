819cee6f036d7ececbe2dbf5f4454cdab5c98678
Connie <clai011@ucr.edu>
2018-04-23 20:24:41 -0700

    loop done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let emailArr;
+/*let emailArr;
 var yourEmail;
 
 while (true) {
@@ -15,6 +15,8 @@ while (true) {
     alert('Thanks for providing a valid email.');
   }
 }
+
+*/
 
 
 //SWITCH STATEMENT - evaluates single parameter
@@ -37,7 +39,7 @@ switch(parseInt(numOfProjects)) {
 */
 
 
-Ternary Operator = short-hand for If-Else
+//Ternary Operator = short-hand for If-Else
 
 // condition ? exp 1 : exp2
 /* let age = prompt("your age?")
@@ -56,3 +58,81 @@ let cars = ["jeep", "subaru", "honda"];
 
 
 //slice() with no argument returns exact copy of array
+
+
+/*
+let shoppingList = [];
+
+let item1="bleach";
+let item2="cheese";
+let item3="crackas"
+let item4="wine";
+
+shoppingList.push(item1, item2, item3, item4);
+
+//console.log(shoppingList);
+
+//NESTED ARRAYS
+
+let a = ["A", '1'];
+let b = ["B", "b"];
+let array =[a, b];
+
+//To access '1' from array a, console.log(array[0][1]);
+*/
+
+
+let m1=["The Lion King", "Simba", "1995"];
+let m2=["Mulan", "mulan",
+"2000"];
+let m3=["Thor", "thor", "2011"];
+let m4=["Captain America", "Steve", "1989"];
+let m5=["MazeRunner", "Thomas", "2013"];
+
+let favoriteMovies=[];
+
+favoriteMovies.push(m1,m2,m3,m4,m5);
+
+let title=[m1[0],m2[0],m3[0],m4[0], m5[0]];
+
+alert(title);
+
+let fave = [m1[0],m5[0]];
+
+
+let length = favoriteMovies.length;
+
+alert(length);
+
+let i = 0;
+
+for(i=0;i<length;i++){
+
+if(favoriteMovies[i][0] === "The Lion King"){
+
+  alert(`${favoriteMovies[i][1]} is my fav movie and it was made in ${favoriteMovies[i][2]}`);
+
+} else if(favoriteMovies[i][0] === "Mulan") {
+
+  alert(`${favoriteMovies[i][1]} is my fav movie and it was made in ${favoriteMovies[i][2]}`);
+
+} else if(favoriteMovies[i][0] === "Thor") {
+
+  alert(`${favoriteMovies[i][1]} is my fav movie and it was made in ${favoriteMovies[i][2]}`);
+
+} else if(favoriteMovies[i][0] === "Captain America") {
+
+  alert(`${favoriteMovies[i][1]} is my fav movie and it was made in ${favoriteMovies[i][2]}`);
+
+} else if(favoriteMovies[i][0] === "MazeRunner") {
+
+  alert(`${favoriteMovies[i][1]} is my fav movie and it was made in ${favoriteMovies[i][2]}`);
+
+} else {
+  alert("TYPO!!!");
+}
+
+
+  alert(favoriteMovies[0][0],favoriteMovies[0][1]);//just titles
+
+}
