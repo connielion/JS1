fa0821f383885f8fd33a64fec9dd4e7a4a784ca9
Connie <clai011@ucr.edu>
2018-04-23 20:59:49 -0700

    function

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//FUNCTION
+/*
+let num = prompt("Enter a number and we'll the square!");
+function squareNum(num){
+  return num*num;
+}
+alert(squareNum(num));
+*/
+
+function greet(name, greeting) {
+
+  return `Hello, ${name}, ${greeting}!`;
+}
+
+let hi = greet("Connie", "Hi!!!");
+alert(hi);
