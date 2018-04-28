f61b9bb08848e15507ddb99eb6c54fb30dea11e1
Connie <clai011@ucr.edu>
2018-04-28 12:24:13 -0700

    not done with pt2

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function square(num){
 
   if(!isNaN(num)){ //if user enters number
 
-    alert(numSquare);
+    alert(`Square of that number is ${numSquare}`);
   }
   else{
     alert("Not a number!");
@@ -20,7 +20,7 @@ square(num); //call function
 In a second function, capitalize the first letter of a string and add a period (.) to the end of the string if it doesn't already end with a period
 Create a string that will flip the position of the first and second half of a string. For example, the string “abcdef” would be returned as “defabc”. Hint: use substring.*/
 
-
+/*
 let str = prompt("Enter a sentence, all lowercased and no period.");
 
 function check(string) {
@@ -28,9 +28,9 @@ function check(string) {
 str.charAt(0) !== str.charAt(0).toLowerCase()   <---first char isn't lowercased
 str.slice(-1) !== "."      <--last char isn't a period
 (str.charAt(0) !== str.charAt(0).toLowerCase()) && (str.slice(-1) == ".") <=capitalized AND last char is '.'
-*/
 
-//check case, period
+
+check case, period
 if() {
 
 } else if {
@@ -43,4 +43,5 @@ return;
 
 }
 
-check();  //function call
+check();  function call
+*/
