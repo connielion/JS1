722987d09d87f383adaae946fda5e3a6f0546f41
Connie <clai011@ucr.edu>
2018-04-28 01:20:30 -0700

    conditions made

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let num = parseInt(prompt("Enter a number."));
+/* let num = parseInt(prompt("Enter a number."));
 let numSquare = num * num;
 
 function square(num){
@@ -13,4 +13,24 @@ function square(num){
 
 }
 
-square(num); //call function
+square(num); //call function*/
+
+/*
+In a second function, capitalize the first letter of a string and add a period (.) to the end of the string if it doesn't already end with a period
+Create a string that will flip the position of the first and second half of a string. For example, the string “abcdef” would be returned as “defabc”. Hint: use substring.*/
+
+
+let str = prompt("Enter a sentence, all lowercased and no period.");
+
+function check(string) {
+/*  CHECKS
+str.charAt(0) !== str.charAt(0).toLowerCase()   <---first char isn't lowercased
+str.slice(-1) !== "."      <--last char isn't a period
+(str.charAt(0) !== str.charAt(0).toLowerCase()) && (str.slice(-1) == ".") <=capitalized AND last char is '.'
+*/
+
+return;
+
+}
+
+check();  //function call
