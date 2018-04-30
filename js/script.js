7af5b93bf1ddb9065e4dd862d3e724b985613e2b
Connie <clai011@ucr.edu>
2018-04-30 11:19:21 -0700

    ugh

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let num = parseInt(prompt("Enter a number."));
+/* let num = parseInt(prompt("Enter a number."));
 
 let numSquare = num * num;
 
@@ -8,10 +8,62 @@ function square(num){
 
     alert(`Square of that number is ${numSquare}`);
   }
-  else if (isNaN(num)){
+  else{
     alert("Not a number!");
+  }
 
-  } 
 }
 
 square(num); //call function
+*/
+/*
+In a second function, capitalize the first letter of a string and add a period (.) to the end of the string if it doesn't already end with a period
+Create a string that will flip the position of the first and second half of a string. For example, the string “abcdef” would be returned as “defabc”. Hint: use substring.*/
+
+/*
+let str = prompt("Enter a sentence, all lowercased and no period.");
+
+function check(string) {
+/*
+
+CHECKS:
+
+str.charAt(0) !== str.charAt(0).toLowerCase()   <---first char isn't lowercased
+
+str.slice(-1) !== "."      <--last char isn't a period
+
+(str.charAt(0) !== str.charAt(0).toLowerCase()) && (str.slice(-1) == ".") <---capitalized AND last char is '.'
+
+
+check case, period
+*/
+
+let str = prompt("Enter a word or sentence without capitalization and period at the end.")
+
+function check(){
+
+
+    if((str.charAt(0) === str.charAt(0).toUpperCase()) && (str.slice(-1) !== ".")) {
+
+      //first: is uppercase; last is not "."
+      return alert((str.slice(0)) + ".");
+
+    } else if ((str.charAt(0) == str.charAt(0).toLowerCase()) && (str.slice(-1) !== ".")){
+        //not capitalized and no period
+      return alert(str.charAt(0).toUpperCase() + str.slice(1) + ".");
+
+    } else if ((str.charAt(0) !== str.charAt(0).toUpperCase()) && (str.slice(-1) !== ".")){
+
+      return alert((str.charAt(0).toUpperCase()) + (str.slice(1)) + ".");
+    }
+    else if ((str.charAt(0) !== str.charAt(0).toUpperCase()) && (str.slice(-1) == ".")){
+      return alert((str.charAt(0).toUpperCase()) + str.slice(1));
+    } else
+    {
+      alert("Nice English.");
+    }
+
+  }
+
+
+check();  //function call
